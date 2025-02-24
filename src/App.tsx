c742a29a4d42047c9118a77dbd6407db10d78098
sungsieon <mari9908@naver.com>
2025-02-25 05:12:15 +0900

    배열 타입

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import './App.css';
 import React from 'react';
 
 function App() {
+  //배열
+  const names: string[] = ["Alice","Bob","Eve"]; 
 
   return (
-    <div>
-    <p>asd</p>
+    <div className="flex">
+      {names.map((n) => (
+        <p key={n}>{n}</p>
+      ))}
     </div>
   );
 }
