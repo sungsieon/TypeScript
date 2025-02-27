8ab52867cf6f820fddbdfe21c067a2972cd72f47
sungsieon <mari9908@naver.com>
2025-02-28 08:42:51 +0900

    타입별칭, 인터페이스 공부

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,43 @@ function App() {
   //배열
   const names: string[] = ["Alice","Bob","Eve"]; 
 
+  function  printcoord(pt: {x:number; y:number}){
+     console.log("x value is " + pt.x )
+     console.log("y value is " + pt.y)
+  }
+  
+  // Array.isArray 함수와 else일 때 type값
+  function welcomePeople(x: string[] | string){
+    if(Array.isArray(x)){
+      console.log("Hello, "+ x.join("and"));
+    }else {
+      console.log("Welcome lone traveler " + x);
+    }
+
+  }
+
+  printcoord({x:10, y:20});
+
+// 타입 별칭
+
+ type User = {
+  age:number;
+  name:string;
+ }
+
+ const user:User = {
+  age:15,
+  name:"bian",
+ }
+
+ type ID = {
+  id:number;
+ }
+
+ const login:ID = {
+  id:15,
+ }
+
   return (
     <div className="flex">
       {names.map((n) => (
